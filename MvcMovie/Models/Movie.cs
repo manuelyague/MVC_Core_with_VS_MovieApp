16754d777f6e79ce488e25ede4b68b275ab5adcd
Manuel Yagüe <manuel.yague.v@outlook.com>
2018-12-02 18:57:31 -0300

    Versión 1.1

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,16 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [Display(Name = "Título")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
+        [Display(Name = "Fecha de estreno")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Género")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Display(Name = "Precio en USD")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [Display(Name = "Clasificación")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
     }
 }
